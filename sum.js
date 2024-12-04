779f6d3b73a6584978140e9a03e3f0ee55acc1aa
incknwu8_amway <incknwu8@amway.com>
2024-12-04 17:32:51 +0530

    added second case

--- sum.js
+++ sum.js
@@ -2,6 +2,7 @@ function add(numbers) {
     if (numbers === "") {
         return 0;
     }
+    return parseInt(numbers, 10);
 }
 
 module.exports = { add };
--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,9 @@
 const { add } = require('./sum');
 
 test('returns 0 for an empty string', () => {
-  expect(add("")).toBe(0);
+    expect(add("")).toBe(0);
+});
+
+test('returns the number itself when a single number is given', () => {
+    expect(add("1")).toBe(1);
 });
